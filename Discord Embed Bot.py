a1ba17c1d92acbded1765cea14d0254907a750aa
rbongocat <43015136+rbongocat@users.noreply.github.com>
2019-04-30 14:25:19 -0700

    Update Discord Embed Bot.py
    very basic version of what i have in mind for the search function

--- Discord Embed Bot.py
+++ Discord Embed Bot.py
@@ -7,10 +7,10 @@ from discord.ext.commands import ConversionError #remembered what I was going to
 from discord.ext import commands
 
 BOT_PREFIX = ("e.")
-
 TOKEN = ""
-
 client = Bot(command_prefix = BOT_PREFIX)
+
+path = ''
 
 @client.command(name = "embed",
                 brief = "Does embedding.",
@@ -23,8 +23,46 @@ async def embed(ctx, sendToChannel: discord.TextChannel, embedTitle, embedLink, 
     embed.set_image(url = embedImage)
     embed.add_field(name = "Guild Name", value = embedTitle, inline = True)
     embed.add_field(name = "Link", value = embedLink, inline = True)
+    
     if embedDesc != None:
         embed.add_field(name = "Description", value = embedDesc, inline = False)
+
+    if len(open(path, 'r').read()) == 0:
+        print(open(path, 'r').read())
+        print("1")
+        embedFile = open(path, 'r+')
+        embedFile.write(str(sendToChannel.id))
+        embedFile.write(embedTitle)
+        embedFile.write(embedLink)
+        embedFile.write(embedImage)
+    else:
+        print("2")
+        embedFile = open(path, 'a')
+        embedFile.write("\n")
+        embedFile.write(str(sendToChannel.id))
+        embedFile.write(embedTitle)
+        embedFile.write(embedLink)
+        embedFile.write(embedImage)
+
+    if embedDesc != None:
+        embedFile.write(embedDesc)
+    embedFile.close()
+    
     await client.get_channel(sendToChannel.id).send(embed = embed)
+
+@client.command(hidden = True,
+                pass_context = True) #pass needed so I can use ctx
+async def cleardb(ctx):
+    if ctx.message.author.id == "156179325421486081" or "150417106549211136": #dream or me
+        embedFile = open(path, 'w')
+        embedFile.write("")
+        channel = ctx.channel
+        await channel.send("Done.")
+
+@client.command(hidden = True,
+                pass_context = True)
+async def dblen(ctx):
+    channel = ctx.channel
+    await channel.send(len(open(path, 'r').read()))
 
 client.run(TOKEN)
