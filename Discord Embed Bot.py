1e0ed79873ec3a9115d95820ef2b623dc5470144
rbongocat <43015136+rbongocat@users.noreply.github.com>
2019-04-30 18:36:06 -0700

    Update Discord Embed Bot.py
    i think final
    
    "path" preferrably being a folder/directory as to where to store the files
    e.g. "C:/Users/<user>/..."

--- Discord Embed Bot.py
+++ Discord Embed Bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 import asyncio
 from discord import *
 from discord.message import *
@@ -7,10 +8,10 @@ from discord.ext.commands import ConversionError #remembered what I was going to
 from discord.ext import commands
 
 BOT_PREFIX = ("e.")
-TOKEN = ""
+TOKEN = "<bot token>"
 client = Bot(command_prefix = BOT_PREFIX)
 
-path = ''
+path = '<add path>'
 
 @client.command(name = "embed",
                 brief = "Does embedding.",
@@ -18,51 +19,90 @@ path = ''
                 description = "Embeds guilds for Dream's sake. Note 'embedDesc' is optional. Requires permission 'manage_messages' to be used.",
                 pass_context = True)
 @commands.has_permissions(manage_messages = True)
-async def embed(ctx, sendToChannel: discord.TextChannel, embedTitle, embedLink, embedImage, embedDesc = None):
+async def embed(ctx, sendToChannel: discord.TextChannel, embedTitle, embedInvite, embedImage, gwCode, embedDesc = None):
     embed = discord.Embed(color = 0x36393E)
     embed.set_image(url = embedImage)
     embed.add_field(name = "Guild Name", value = embedTitle, inline = True)
-    embed.add_field(name = "Link", value = embedLink, inline = True)
+    embed.add_field(name = "Link", value = embedInvite, inline = True)
     
     if embedDesc != None:
         embed.add_field(name = "Description", value = embedDesc, inline = False)
 
-    if len(open(path, 'r').read()) == 0:
-        print(open(path, 'r').read())
-        print("1")
-        embedFile = open(path, 'r+')
-        embedFile.write(str(sendToChannel.id))
-        embedFile.write(embedTitle)
-        embedFile.write(embedLink)
-        embedFile.write(embedImage)
-    else:
-        print("2")
-        embedFile = open(path, 'a')
-        embedFile.write("\n")
-        embedFile.write(str(sendToChannel.id))
-        embedFile.write(embedTitle)
-        embedFile.write(embedLink)
-        embedFile.write(embedImage)
+    newSpace = "\n"
 
-    if embedDesc != None:
-        embedFile.write(embedDesc)
-    embedFile.close()
+    newPath = (path + embedTitle + ".txt")
+
+    pathCheck = os.path.isfile(newPath)
+
+    if pathCheck == True:
+        await ctx.channel.send("Path already exists. Pick another name or delete the file.")
+    elif pathCheck == False:
+        open(path + embedTitle + ".txt", 'x')
+        print(newPath)
+
+        embedFile = open(newPath, 'w')
+        embedFile.write("Guild Name: " + embedTitle + newSpace)
+        embedFile.write("Invite Link: " + "<" + embedInvite + ">" + newSpace)
+        embedFile.write("GW Code: " + gwCode.lower() + newSpace)
+        embedFile.write(embedImage + newSpace)
+
+        if embedDesc != None:
+            embedFile.write("Desc: " + embedDesc)
+        embedFile.close()
     
     await client.get_channel(sendToChannel.id).send(embed = embed)
 
 @client.command(hidden = True,
                 pass_context = True) #pass needed so I can use ctx
-async def cleardb(ctx):
+async def cleardb(ctx, flag = None):
     if ctx.message.author.id == "156179325421486081" or "150417106549211136": #dream or me
-        embedFile = open(path, 'w')
-        embedFile.write("")
-        channel = ctx.channel
-        await channel.send("Done.")
+        if flag == None:
+            await ctx.channel.send("Clearing all data...")
+            for bdir, dirs, files in os.walk(path):
+                for fname in files:
+                    print(bdir, fname)
+                    os.remove(path + fname)
+            await ctx.channel.send("Done.")
+        elif flag != None:
+            await ctx.channel.send("Clearing specific file...")
+            os.remove(path + flag + ".txt")
+            await ctx.channel.send("Done.")
 
 @client.command(hidden = True,
                 pass_context = True)
-async def dblen(ctx):
-    channel = ctx.channel
-    await channel.send(len(open(path, 'r').read()))
+@commands.has_permissions(manage_messages = True)
+async def listdb(ctx):
+    for bdir, dirs, files in os.walk(path):
+        for fname in files:
+            if fname.endswith(".txt"):
+                fname = fname[:-4]
+                await ctx.channel.send(fname) #fname being file name
+
+@client.command(pass_context = True)
+async def search(ctx, fileName):
+    pathCheck = os.path.isfile(path + fileName + ".txt")
+    checkCount = 0
+    errorCount = 0
+    if pathCheck == True:
+        await ctx.channel.send((open(path + fileName + ".txt", 'r').read()))
+    elif pathCheck == False:
+        for bdir, dirs, files in os.walk(path):
+                for fname in files:
+                    codeCheck = open(path + fname)
+                    #print("1") these are all debug prints, kinda like laps. if it prints the number, shows me how far it got before looping back
+                    for lineNum, linePrint in enumerate(codeCheck):
+                        #print("2")
+                        if lineNum == 2:
+                            #print("3")
+                            if fileName.lower() in linePrint:
+                                checkCount += 1
+                                if checkCount > 0:
+                                    await ctx.channel.send((open((path + fname), 'r').read()))
+                                    await asyncio.sleep(0.5) #prevent rate limiting (i think it's 5 inputs per like, 0.5s?)
+                            elif fileName.lower() not in linePrint:
+                                errorCount += 1
+                                if errorCount == len(files):
+                                    await ctx.channel.send("Invalid file name/GW code.")
+    
 
 client.run(TOKEN)
